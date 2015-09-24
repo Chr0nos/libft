241a0676373b3a468c03b8d7c7ef5f62dec30585
Chr0nos <hisoka2501@free.fr>
2015-09-24 18:26:33 +0000

    ajout de ft_putstr_align_right

--- srcs/ft_putstr.c
+++ srcs/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/12 11:02:21 by snicolet          #+#    #+#             */
-/*   Updated: 2015/08/26 14:22:25 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/09/24 17:25:23 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,16 @@ void	ft_putstr(char *str)
 void	ft_putstr_error(char *str)
 {
 	write(2, str, ft_strlen(str));
+}
+
+void	ft_putstr_align_right(char *str, char fill, t_size len)
+{
+	t_size	left;
+	t_size	size;
+
+	size = ft_strlen(str);
+	left = len - size;
+	while (left-- > 0)
+		ft_putchar(fill);
+	write(1, str, len);
 }

8f4b450580d7f42d384c6d39cd6c0fd99de7951a
Chr0nos <hisoka2501@free.fr>
2015-10-01 23:24:57 +0000

    ft_btree_add prends mtn un pointeur de pointeur

--- srcs/btree/ft_btree_add.c
+++ srcs/btree/ft_btree_add.c
@@ -6,34 +6,36 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/01 15:53:00 by snicolet          #+#    #+#             */
-/*   Updated: 2015/10/01 20:58:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/10/01 23:24:03 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "btree.h"
 
-void	ft_btree_add(t_btree *root, void *data, int (*f)())
+void	ft_btree_add(t_btree **root, void *data, int (*f)())
 {
 	t_btree	*x;
 
-	if (f(root->data, data) <= 0)
+	if (!root)
+		*root = ft_btree_create_node(data);
+	else if (f((*root)->data, data) <= 0)
 	{
-		if (root->left)
+		if ((*root)->left)
 		{
-			if (f(root->left->data, data) > 0)
+			if (f((*root)->left->data, data) > 0)
 			{
-				x = root->left;
-				root->left = ft_btree_create_node(data);
-				root->left->left = x;
+				x = (*root)->left;
+				(*root)->left = ft_btree_create_node(data);
+				(*root)->left->left = x;
 			}
 			else
-				ft_btree_add(root->left, data, f);
+				ft_btree_add(&(*root)->left, data, f);
 		}
 		else
-			root->left = ft_btree_create_node(data);
+			(*root)->left = ft_btree_create_node(data);
 	}
-	else if (root->right)
-		ft_btree_add(root->right, data, f);
+	else if ((*root)->right)
+		ft_btree_add(&(*root)->right, data, f);
 	else
-		root->right = ft_btree_create_node(data);
+		(*root)->right = ft_btree_create_node(data);
 }

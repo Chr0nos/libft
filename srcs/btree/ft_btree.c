9d78fc06c6181f25d0602efbf736342e5f4c5a78
Chr0nos <hisoka2501@free.fr>
2015-10-01 16:09:57 +0000

    verification que le malloc de ft_btree_create_node se soit bien passe

--- srcs/btree/ft_btree.c
+++ srcs/btree/ft_btree.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/10 23:34:34 by snicolet          #+#    #+#             */
-/*   Updated: 2015/09/10 23:39:14 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/10/01 16:09:37 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_btree	*ft_btree_create_node(void *data)
 	t_btree	*root;
 
 	root = malloc(sizeof(t_btree));
+	if (!root)
+		return (0);
 	root->left = 0;
 	root->right = 0;
 	root->data = data;

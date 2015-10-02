6745429f220486eef8d53f946d99c1a8d0d7a5d5
Chr0nos <hisoka2501@free.fr>
2015-10-02 19:48:58 +0000

    optimisation de ft_btree_search qui cherche maintenant en tenant compte des contraintes de l arbre binaire, ft_btree_search_mess permet de chercher dans un arbre non trie

--- includes/btree.h
+++ includes/btree.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/09/10 18:57:39 by snicolet          #+#    #+#             */
-/*   Updated: 2015/10/02 16:30:10 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/10/02 19:48:05 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ void				ft_btree_foreach_prefix(t_btree *root, void (*f)(void *));
 void				ft_btree_foreach_suffix(t_btree *root, void (*f)(void *));
 t_btree				*ft_btree_create_node(void *data);
 void				ft_btree_add(t_btree **root, void *data, int (*f)());
-t_btree				*ft_btree_search(t_btree *root, void *val, int (*f)());
+t_btree				*ft_btree_search_mess(t_btree *root, void *val, int (*f)());
+t_btree				*ft_btree_search(t_btree *root, void *data, int (*f)());
 void				ft_btree_insert(t_btree **node, void *data, int (*f)());
 int					ft_btree_level(t_btree *root, t_btree *item, int level);
 
--- srcs/btree/ft_btree_search.c
+++ srcs/btree/ft_btree_search.c
@@ -6,21 +6,36 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/02 12:46:07 by snicolet          #+#    #+#             */
-/*   Updated: 2015/10/02 12:56:25 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/10/02 19:47:34 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "btree.h"
 
-t_btree		*ft_btree_search(t_btree *root, void *val, int (*f)())
+t_btree		*ft_btree_search_mess(t_btree *root, void *val, int (*f)())
 {
 	if (!root)
 		return (0);
 	if (f(root->data, val) == 0)
 		return (root);
 	if (root->left)
-		ft_btree_search(root->left, val, f);
+		return (ft_btree_search_mess(root->left, val, f));
 	if (root->right)
-		ft_btree_search(root->right, val, f);
+		return (ft_btree_search_mess(root->right, val, f));
 	return (0);
+}
+
+t_btree		*ft_btree_search(t_btree *root, void *val, int (*f)())
+{
+	int		r;
+
+	if (!root)
+		return (0);
+	r = f(root->data, val);
+	if (r == 0)
+		return (root);
+	else if (r < 0)
+		return (ft_btree_search(root->left, val, f));
+	else
+		return (ft_btree_search(root->right, val, f));
 }

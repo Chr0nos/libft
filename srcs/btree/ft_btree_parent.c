03af7210741b794fbf1f98c377054c0c27e25acf
Chr0nos <hisoka2501@free.fr>
2015-10-07 14:21:48 +0000

    ajout de ft_list_parent

---
+++ srcs/btree/ft_btree_parent.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_btree_parent.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/03 11:28:56 by snicolet          #+#    #+#             */
+/*   Updated: 2015/10/03 11:33:51 by snicolet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "btree.h"
+
+t_btree		*ft_btree_parent(t_btree *root, t_btree *item)
+{
+	if (!root)
+		return (0);
+	if ((root->left == item) || (root->right == item))
+		return (root);
+	if (root->left)
+		return (ft_btree_parent(root->left, item));
+	if (root->right)
+		return (ft_btree_parent(root->right, item));
+	return (0);
+}

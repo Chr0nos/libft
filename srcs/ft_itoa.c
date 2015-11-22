9f9fae5710a9423350286342a61ce577286b6f3a
Sébastien N <snicolet95@gmail.com>
2015-11-22 19:08:05 +0000

    changement du comportement de itoa: desormais standard et creation de ft_itobase pour l ancien comportement

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -12,10 +12,33 @@
 
 #include "ft.h"
 
-char	*ft_itoa(int nb, char *buffer, unsigned char base)
+char	*ft_itoa(int nb, char *buffer)
 {
 	int			p;
-	const char	*map = "0123456789abcdefghijklmnopqrstuvwxyz";
+	const char	*map = "0123456789";
+
+	p = 0;
+	if (nb == 0)
+		buffer[p++] = '0';
+	if (nb < 0)
+	{
+		buffer[p++] = '-';
+		nb *= -1;
+	}
+	while (nb)
+	{
+		buffer[p] = map[nb % 10];
+		nb /= 10;
+		p++;
+	}
+	buffer[p] = '\0';
+	ft_strrev(buffer + ((buffer[0] == '-') ? 1 : 0));
+	return (buffer);
+}
+
+char	*ft_itobase(int nb, char *buffer, unsigned char base, const char *map)
+{
+	int			p;
 
 	if (base < 2)
 		return (0);

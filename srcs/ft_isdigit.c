fd45cc912c14c7bd128be0ffce2e3e6200433809
Chr0nos <hisoka2501@free.fr>
2015-11-27 01:11:55 +0000

    clarification de ft_isdigit

--- srcs/ft_isdigit.c
+++ srcs/ft_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/26 14:01:58 by snicolet          #+#    #+#             */
-/*   Updated: 2015/11/26 14:03:08 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/11/27 01:11:28 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@ int		ft_isdigit(const char *str)
 {
 	while ((*str) && ((*str >= '0') && (*str <= '9')))
 		str++;
-	return ((*str ? 0 : 1));
+	return ((*str != '\0' ? 0 : 1));
 }

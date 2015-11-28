2689119728dfc380387c5decf1e26c996e2994a9
Chr0nos <snicolet@student.42.fr>
2015-11-28 23:52:55 +0100

    debug

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 20:14:55 by snicolet          #+#    #+#             */
-/*   Updated: 2015/11/28 23:22:20 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/11/28 23:51:19 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@ size_t	ft_strlcat(char *dest, const char *src, size_t size)
 	size_t			len;
 	size_t			p;
 
-	len = ft_strlen(src) + dlen;
+	len = ft_strlen(src);
 	if (len > size)
-		len = size;
+	len = size;
+	if (len == size)
+		return (len + size);
 	dest += dlen;
 	p = 0;
-	while ((src[p]) && (p < len + 1))
+	while ((src[p]) && (p < size - len - 1))
 	{
 		dest[p] = src[p];
 		p++;
 	}
 	dest[p++] = '\0';
-	return (len);
+	return (len + dlen);
 }

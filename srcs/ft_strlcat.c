989db801dfb3f8b52e679d96b700de6f249e3c8c
Chr0nos <hisoka2501@free.fr>
2015-11-29 14:34:06 +0000

    ft_strlcat

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,24 +6,27 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 20:14:55 by snicolet          #+#    #+#             */
-/*   Updated: 2015/11/28 23:51:19 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/11/29 14:33:54 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "string.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
+static size_t	ft_max(size_t nb, size_t max)
+{
+	return (nb > max ? max : nb);
+}
+
+size_t			ft_strlcat(char *dest, const char *src, size_t size)
 {
 	const size_t	dlen = ft_strlen(dest);
 	size_t			len;
 	size_t			p;
 
-	len = ft_strlen(src);
-	if (len > size)
-	len = size;
+	len = ft_max(ft_strlen(src), size);
 	if (len == size)
-		return (len + size);
+		return (len + dlen);
 	dest += dlen;
 	p = 0;
 	while ((src[p]) && (p < size - len - 1))

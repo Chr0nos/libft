ef584ffbe00a6e202ae1c591d30e4c70dad863b9
Chr0nos <hisoka2501@free.fr>
2015-11-29 15:40:06 +0000

    strlcat, encore et toujours

--- srcs/ft_strlcat.c
+++ srcs/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 20:14:55 by snicolet          #+#    #+#             */
-/*   Updated: 2015/11/29 14:33:54 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/11/29 15:39:43 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ static size_t	ft_max(size_t nb, size_t max)
 
 size_t			ft_strlcat(char *dest, const char *src, size_t size)
 {
-	const size_t	dlen = ft_strlen(dest);
+	const size_t	dlen = ft_max(ft_strlen(dest), size);
 	size_t			len;
 	size_t			p;
 
-	len = ft_max(ft_strlen(src), size);
-	if (len == size)
-		return (len + dlen);
+	if (dlen == size--)
+		return (dlen + dlen);
 	dest += dlen;
+	len = ft_strlen(src);
 	p = 0;
-	while ((src[p]) && (p < size - len - 1))
+	while ((src[p]) && (p < size - dlen))
 	{
 		dest[p] = src[p];
 		p++;
 	}
-	dest[p++] = '\0';
+	dest[p] = '\0';
 	return (len + dlen);
 }

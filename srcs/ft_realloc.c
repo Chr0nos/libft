75c6d1f8ebd00aeadfaa69d737a89eb2285e8178
Chr0nos <snicolet@student.42.fr>
2015-12-02 13:20:27 +0100

    includes

--- srcs/ft_realloc.c
+++ srcs/ft_realloc.c
@@ -6,11 +6,12 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/25 12:07:53 by snicolet          #+#    #+#             */
-/*   Updated: 2015/08/25 12:27:21 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/02 12:48:31 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	*ft_realloc(void *ptr, unsigned int size, unsigned int new_size)
 {

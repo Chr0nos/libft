1711e76d47b1bb118aa71cc2db0dc77c06f55257
Chr0nos <snicolet@student.42.fr>
2015-12-02 20:54:12 +0100

    fix strnstr

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 00:43:43 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/02 20:45:14 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/02 20:53:28 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ char	*ft_strnstr(const char *s1, const char *s2, size_t n)
 
 	if (!*s2)
 		return ((char*)s1);
-	if (n < len)
-		return (NULL);
-	while ((*s1) & (n--))
+	while ((*s1) && (n))
 	{
+		n--;
 		if (ft_strncmp(s1, s2, len) == 0)
 			return ((char*)s1);
 		s1++;

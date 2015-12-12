68eb92b425696d09441486467d20a2cf112fb9ec
Chr0nos <snicolet@student.42.fr>
2015-12-12 15:50:07 +0100

    opti memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 13:40:07 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/03 16:40:20 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/12 15:49:46 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	const unsigned char	*s;
 	unsigned char		*d;
 
+	if (dest == src)
+		return (dest);
 	if ((dest < src) || (dest > src + n))
 		return (ft_memcpy(dest, (void*)src, n));
 	s = src;

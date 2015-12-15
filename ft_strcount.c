db77c6dabb1538728fd2714051bbb1b935edaf88
Chr0nos <snicolet@student.42.fr>
2015-12-15 18:06:54 +0100

    ajout du include de string dans strcount

--- ft_strcount.c
+++ ft_strcount.c
@@ -6,11 +6,12 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/17 14:04:10 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/15 17:55:54 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/15 18:06:36 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <string.h>
 
 size_t	ft_strcount(char *str, char c)
 {

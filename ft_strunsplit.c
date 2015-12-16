e536ecd452d9cfefd5a879bf83ba1913b1d1d22b
Chr0nos <snicolet@student.42.fr>
2015-12-16 20:48:13 +0100

    norme

--- ft_strunsplit.c
+++ ft_strunsplit.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/16 15:06:23 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/16 19:59:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2015/12/16 20:47:44 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ char	*ft_strunsplit(char **tab, char separator)
 	size = 0;
 	while (tab[size])
 		size++;
-	if (!(str = (char*)malloc(sizeof(char) * (ft_tabstrlen(tab)  + size + 1))))
+	if (!(str = (char*)malloc(sizeof(char) * (ft_tabstrlen(tab) + size + 1))))
 		return (NULL);
 	p = 0;
 	slen = 0;

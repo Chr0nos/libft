a96b0325effb848c9c36196e6f37d560be7d9a16
Chr0nos <snicolet@student.42.fr>
2016-02-07 21:11:59 +0100

    la fonction ft_lstadd renvoi desormais le pointeur de l item ajoute: pour rendre plus coherent avec ft_lstpush_back et permetre des pointeurs sur fonctions en les utilisants

--- ft_lstadd.c
+++ ft_lstadd.c
@@ -6,14 +6,15 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 17:55:07 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/01 18:11:18 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/02/07 21:10:14 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd(t_list **alst, t_list *new)
+t_list	*ft_lstadd(t_list **alst, t_list *new)
 {
 	new->next = *alst;
 	*alst = new;
+	return (new);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/17 10:32:34 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/05 19:29:24 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/02/07 21:10:40 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,7 +138,7 @@ t_list				*ft_lstnewlink(void *content, size_t content_size);
 void				ft_lstremove(t_list **item, t_list **root, void (*f)());
 void				ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
 void				ft_lstdel(t_list **ast, void (*del)(void *, size_t));
-void				ft_lstadd(t_list **alst, t_list *new);
+t_list				*ft_lstadd(t_list **alst, t_list *new);
 void				ft_lstinsert(t_list **parent, t_list *item);
 void				ft_lstiter(t_list *lst, void (*f)(t_list *elem));
 size_t				ft_lststrlen(t_list *lst);

27004d0871b1a549eb7318e5eb7d464186ca320d
Sébastien Nicolet <snicolet@student.42.fr>
2016-02-24 15:22:07 +0100

    fix de ft_ftobuff sur les nonbres negatifs

--- ft_ftobuff.c
+++ ft_ftobuff.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/24 14:48:05 by snicolet          #+#    #+#             */
-/*   Updated: 2016/02/24 14:59:20 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/02/24 15:21:40 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,11 @@ int		ft_ftobuff(char *buffer, float nb, unsigned int base, const char *map)
 
 	real = (int)nb;
 	real_len = ft_itobuff(buffer, real, base, map);
-	nb -= (float)real;
-	nb *= 1000000000;
+	nb = ((nb) - (float)real) * 1000000;
 	if ((int)nb)
 	{
 		buffer[real_len++] = '.';
-		ft_itobuff(buffer + real_len, (int)nb, base, map);
+		ft_itobuff(buffer + real_len, (int)((nb < 0) ? -nb : nb), base, map);
 	}
 	return (real_len);
 }

b7fe8eb9fb04389d0e49a87dbf4a6de6c4d5cc18
Sebastien N <snicolet@student.42.fr>
2016-03-11 19:45:19 +0100

    optimisation de ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 12:17:00 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/13 16:20:08 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/03/11 19:45:03 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,19 @@
 
 void	*ft_memset(void *dest, int c, size_t n)
 {
-	size_t	p;
+	size_t			p;
+	unsigned long	blk;
 
 	p = 0;
-	while (p < n)
+	while ((p < n) && ((n - p) % 8))
 		((unsigned char *)(dest))[p++] = (unsigned char)c;
+	blk = (unsigned long)c | (unsigned long)(c << 8);
+	blk |= blk << 16;
+	blk |= blk << 32;
+	while (p < n)
+	{
+		*(unsigned long*)((unsigned long)dest + p) = blk;
+		p += 8;
+	}
 	return (dest);
 }

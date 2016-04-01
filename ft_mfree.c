8bb09290d51561572aef0e16ee0c26e31fbcbd5d
Sebastien N <snicolet@student.42.fr>
2016-04-01 20:57:21 +0200

    ajout d une verification pour ne pas free les pointeus nulls

--- ft_mfree.c
+++ ft_mfree.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/01 18:56:07 by snicolet          #+#    #+#             */
-/*   Updated: 2016/04/01 19:29:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/04/01 20:56:59 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,17 @@
 int		ft_mfree(unsigned int size, ...)
 {
 	va_list		ap;
+	void		*ptr;
 
 	if (!size)
 		return (0);
 	va_start(ap, size);
 	while (size--)
-		free(va_arg(ap, void*));
+	{
+		ptr = va_arg(ap, void*);
+		if (ptr)
+			free(ptr);
+	}
 	va_end(ap);
 	return (1);
 }

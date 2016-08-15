02ec0dfd4e9978b92409f15faebf565263ea276e
Sebastien N <snicolet@student.42.fr>
2016-08-15 22:44:35 +0200

    avancecement opti strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/24 20:14:55 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/14 14:10:29 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/15 20:11:37 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,21 @@ size_t			ft_strlcat(char *dest, const char *src, size_t size)
 {
 	const size_t	dlen = ft_strlen(dest);
 	const size_t	len = ft_strlen(src);
-	size_t			total;
-	size_t			p;
+	size_t			copy;
+	//size_t			p;
 
-	if (size < dlen)
-		total = len + size;
+	//p = dlen;
+	//while ((*src) && (p + 1 < size))
+	//	dest[p++] = *(src++);
+	//dest[p] = '\0';
+
+	if (dlen + len > size - 1)
+	{
+		copy = size - dlen - 1;
+		ft_memcpy(&dest[dlen], src, copy);
+		dest[dlen + copy] = '\0';
+	}
 	else
-		total = len + dlen;
-	p = dlen;
-	while ((*src) && (p + 1 < size))
-		dest[p++] = *(src++);
-	dest[p] = '\0';
-	return (total);
+		ft_memcpy(&dest[dlen], src, len + 1);
+	return ((size < dlen) ? len + size : len + dlen);
 }

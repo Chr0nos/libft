a20547f32599a02b0167fdf60f3a09f6d7b742d4
Sebastien NICOLET <snicolet@student.42.fr>
2016-08-29 16:28:14 +0200

    norme

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/12 11:04:03 by snicolet          #+#    #+#             */
-/*   Updated: 2016/08/15 20:39:15 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/08/29 16:25:27 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 #include <string.h>
 
 /*
-size_t	ft_strlen(const char *str)
-{
-	const unsigned long		*x = (const unsigned long *)(unsigned long)str;
-	const char				*cp;
-
-	while (42)
-	{
-		if (LONGCHR_NULL(*(x++)))
-		{
-			cp = (const char *)(x - 1);
-			IFRET__(!cp[0], (size_t)(cp - str));
-			IFRET__(!cp[1], (size_t)(cp - str + 1));
-			IFRET__(!cp[2], (size_t)(cp - str + 2));
-			IFRET__(!cp[3], (size_t)(cp - str + 3));
-			IFRET__(!cp[4], (size_t)(cp - str + 4));
-			IFRET__(!cp[5], (size_t)(cp - str + 5));
-			IFRET__(!cp[6], (size_t)(cp - str + 6));
-			IFRET__(!cp[7], (size_t)(cp - str + 7));
-		}
-	}
-}
+** size_t	ft_strlen(const char *str)
+** {
+** 	const unsigned long		*x = (const unsigned long *)(unsigned long)str;
+** 	const char				*cp;
+**
+** 	while (42)
+** 	{
+** 		if (LONGCHR_NULL(*(x++)))
+** 		{
+** 			cp = (const char *)(x - 1);
+** 			IFRET__(!cp[0], (size_t)(cp - str));
+** 			IFRET__(!cp[1], (size_t)(cp - str + 1));
+** 			IFRET__(!cp[2], (size_t)(cp - str + 2));
+** 			IFRET__(!cp[3], (size_t)(cp - str + 3));
+** 			IFRET__(!cp[4], (size_t)(cp - str + 4));
+** 			IFRET__(!cp[5], (size_t)(cp - str + 5));
+** 			IFRET__(!cp[6], (size_t)(cp - str + 6));
+** 			IFRET__(!cp[7], (size_t)(cp - str + 7));
+** 		}
+** 	}
+** }
 */
 
 size_t	ft_strlen(const char *str)
@@ -46,4 +46,3 @@ size_t	ft_strlen(const char *str)
 		;
 	return ((size_t)(end - str - 1));
 }
-

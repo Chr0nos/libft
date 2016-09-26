30ca817a893dd0c6d91206b15643c90dd909c012
Sébastien N <snicolet@student.42.fr>
2016-09-26 21:24:38 +0200

    avancement sur printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/26 15:04:59 by snicolet          #+#    #+#             */
-/*   Updated: 2016/09/26 00:12:49 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/09/26 21:24:27 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 #include "ft_printf.h"
 #include <stdarg.h>
 #include <unistd.h>
+
+static int				ft_printf_convert_int(t_printf *pf)
+{
+	char			buff[13];
+	int				len;
+	const int		max = (pf->flags & FT_PRINTF_PREC) ? pf->precision : 13;
+
+	len = ft_itobuff(buff, va_arg(*pf->ap, int), 10, "0123456789");
+	if (len > max)
+		len = max;
+	memcpy(pf->buff_start, buff, len + 1);
+	return (len);
+}
 
 // test de format %#.8d%
 
@@ -23,21 +36,25 @@ static int				ft_printf_poopinator(const char *str, int len,
 	const size_t	pos = (size_t)pf->buff_start - (size_t)pf->buffer;
 	const size_t	space_left = FT_PRINTF_BSIZE - pos;
 
+	//si la string a faire est plus grande que la taille du buffer on la write
+	//en une seule fois
 	if (FT_PRINTF_BSIZE <= len)
-		write(1, str, len);
-	else if (len > space_left)
+		write(1, str, (size_t)len);
+	//si le buffer n a pas assez de place pour acceuilir la chaine, on le flush
+	else if ((size_t)len > space_left)
 	{
 		write(1, pf->buffer, pos);
 		pf->buff_start = pf->buffer;
 		pf->buffer[0] = '\0';
 	}
+	pf->total_len += (size_t)len;
+	return (0);
 }
 
 static int				ft_printf_engine(const char *s, t_printf *pf)
 {
 	int		pos;
 
-	(void)pf;
 	pos = 0;
 	while (*s)
 	{
@@ -50,6 +67,7 @@ static int				ft_printf_engine(const char *s, t_printf *pf)
 		write(1, "[", 1);
 		write(1, s, (size_t)pos);
 		write(1, "]", 1);
+		ft_printf_poopinator(s, pos, pf);
 		s += pos + 1;
 	}
 	return (pos);
@@ -62,6 +80,7 @@ static void				ft_printf_init(t_printf *pf, va_list *ap)
 	pf->flags = 0;
 	pf->precision = 0;
 	pf->buff_start = pf->buffer;
+	pf->total_len = 0;
 }
 
 int						ft_printf(const char *str, ...)
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/15 17:53:23 by snicolet          #+#    #+#             */
-/*   Updated: 2016/09/25 21:33:59 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/09/26 21:24:20 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,15 @@
 # include <stdarg.h>
 # define FT_PRINTF_BSIZE		2048
 # define FT_PRINTF_CONVERTS		"sSpdDioOuUxXcC"
+# define FT_PRINTF_FLAGS		"#0-+ "
+# define FT_PRINTF_MODIFIERS	"#hlL"
 
 /*
 ** ft_printf flags
 */
 
-# define FT_PRINTF_FLAGS		5
+# define FT_PRINTF_FLAGSNUM		5
+# define FT_PRINTF_CONVERTS		14
 
 # define FT_PRINTF_FLAG_DIESE	(1u << 0)
 # define FT_PRINTF_FLAG_ZERO	(1u << 1)
@@ -34,22 +37,22 @@
 # define FT_PRINTF_FLAG_LL		(1u << 8)
 # define FT_PRINTF_FLAG_J		(1u << 9)
 # define FT_PRINTF_FLAG_Z		(1u << 10)
+# define FT_PRINTF_PREC			(1u << 11)
 
 typedef struct		s_printf
 {
 	va_list			*ap;
 	char			buffer[FT_PRINTF_BSIZE];
 	char			*buff_start;
-	int				bpos;
 	unsigned int	flags;
-	unsigned int	precision;
-	unsigned int	padding;
+	int				precision;
+	size_t			total_len;
 }					t_printf;
 
 typedef struct		s_printf_convert
 {
 	int				letter;
-	int				(*convert)(void *, t_printf *);
+	int				(*convert)(t_printf *);
 }					t_printf_convert;
 
 typedef struct		s_printf_cfg
@@ -60,12 +63,14 @@ typedef struct		s_printf_cfg
 	int				padding;
 }					t_printf_cfg;
 
-static const t_printf_cfg	g_printf_cfg[FT_PRINTF_FLAGS] = {
+static const t_printf_cfg	g_printf_cfg[FT_PRINTF_FLAGSNUM] = {
 	(t_printf_cfg){'#', FT_PRINTF_FLAG_DIESE, 0, 0},
 	(t_printf_cfg){'0', FT_PRINTF_FLAG_ZERO, 0, 0},
 	(t_printf_cfg){'-', FT_PRINTF_FLAG_LESS, 0, FT_PRINTF_FLAG_MORE},
 	(t_printf_cfg){'+', FT_PRINTF_FLAG_MORE, 0, FT_PRINTF_FLAG_LESS},
 	(t_printf_cfg){' ', FT_PRINTF_FLAG_SPACE, 0, 0}
 };
+
+static const t_printf_converts	g_printf_conv[
 
 #endif

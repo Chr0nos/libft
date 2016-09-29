e7d3803ff91dddb312885d4d60910dc9395998e3
Sébastien N <snicolet@student.42.fr>
2016-09-29 22:12:33 +0200

    maj printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/26 15:04:59 by snicolet          #+#    #+#             */
-/*   Updated: 2016/09/29 02:08:54 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/09/29 18:40:12 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,10 @@ static void				ft_printf_append(t_printf *pf, const char *data,
 	pf->space_left -= len;
 }
 
+/*
+** Convert functions
+*/
+
 void					ft_printf_convert_int(t_printf *pf)
 {
 	char			buff[13];
@@ -73,6 +77,10 @@ void					ft_printf_convert_str(t_printf *pf)
 	ft_printf_append(pf, str, ft_strlen(str));
 }
 
+/*
+** (flag / modifier / precision) loaders functions
+*/
+
 static int				ft_printf_loadflags(t_printf *pf, const char c)
 {
 	int		p;
@@ -87,7 +95,8 @@ static int				ft_printf_loadflags(t_printf *pf, const char c)
 	return (1);
 }
 
-static size_t			ft_printf_loadmodifiers(t_printf *pf, const char *str)
+static size_t			ft_printf_loadmodifiers(t_printf *pf, const char *str,
+		int len)
 {
 	int						p;
 	const t_printf_modif	*m = (const t_printf_modif*)&g_printf_modifiers;
@@ -95,12 +104,32 @@ static size_t			ft_printf_loadmodifiers(t_printf *pf, const char *str)
 	p = FT_PRINTF_MODIFIERS;
 	while ((p--) && (!ft_strncmp(m[p].modifier, str, m[p].len)))
 		;
-	if (p < 0)
+	if ((p < 0) || (len < (int)m[p].len))
 		return (0);
 	pf->flags |= g_printf_modifiers[p].flag;
 	pf->flags &= g_printf_modifiers[p].mask;
 	return (g_printf_modifiers[p].len);
 }
+
+static size_t			ft_printf_loadprecision(t_printf *pf, const char *s)
+{
+	size_t		p;
+
+	ft_putendl("TA MERE");
+	if ((*s != '.') || (!s[1]) || (!ft_isdigit(s[1])))
+		return (0);
+	s++;
+	pf->precision = ft_atoi(s);
+	p = 1;
+	while (ft_isdigit(s[p]))
+		p++;
+	return (p);
+}
+
+/*
+** this function apply the convert to the current convert mode,
+** if the convert mode is unknow then the char is printed
+*/
 
 static void				ft_printf_conv(t_printf *pf, const char c)
 {
@@ -115,31 +144,37 @@ static void				ft_printf_conv(t_printf *pf, const char c)
 			return ;
 		}
 	}
+	ft_printf_append(pf, &c, 1);
 }
 
 /*
 ** called on each sub chain delimited by %
+** return the next position to start searching the next % char
 */
 
-static void				ft_printf_exec(const char *str, int len,
+static const char			*ft_printf_exec(const char *str, int len,
 	t_printf *pf)
 {
-	size_t		seek;
+	size_t			seek;
 
 	pf->flags = 0;
-	while (!ft_strany(*str, FT_PRINTF_CONVERTS))
+	seek = 0;
+	while ((len > 0) && (!ft_strany(*str, FT_PRINTF_CONVERTS)))
 	{
-		if ((!ft_printf_loadflags(pf, *str)) &&
-			(seek = ft_printf_loadmodifiers(pf, str) > 1))
-		{
-			str += seek - 1;
-			len -= (int)seek;
-		}
-		str++;
-		len--;
+		if ((seek = (size_t)ft_printf_loadflags(pf, *str)) > 0)
+			;
+		else if ((seek = ft_printf_loadmodifiers(pf, str, len)) > 0)
+			;
+		else if ((seek = ft_printf_loadprecision(pf, str)) > 0)
+			;
+		else
+			break ;
+		str += seek;
+		len -= (int)seek;
 	}
-	if (*str)
-		ft_printf_conv(pf, *(str++));
+	if ((len >= 0) && (*str))
+		ft_printf_conv(pf, *str);
+	return (str + 1);
 }
 
 static void				ft_printf_engine(const char *s, t_printf *pf)
@@ -149,15 +184,10 @@ static void				ft_printf_engine(const char *s, t_printf *pf)
 	pos = (int)ft_strsublen(s, '%');
 	ft_printf_append(pf, s, (size_t)pos);
 	s += pos + 1;
-	while (*s)
+	while ((*s) && ((pos = (int)ft_strchrpos(s, '%')) >= 0))
 	{
-		pos = (int)ft_strsublen(s, '%');
 		ft_printf_append(pf, "][", 2);
-		if (pos)
-			ft_printf_exec(s, pos - 1, pf);
-		else
-			ft_printf_append(pf, "%", 1);
-		s += pos + 1;
+		s = ft_printf_exec(s, pos - 1, pf);
 	}
 	if (*s)
 		ft_printf_append(pf, s, ft_strlen(s));

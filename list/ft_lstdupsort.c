b446fc6ce0dce69751261945fc7eb8fb7d0d80a8
Sebastien NICOLET <snicolet@student.42.fr>
2016-12-03 19:37:33 +0100

    commentaires

--- list/ft_lstdupsort.c
+++ list/ft_lstdupsort.c
@@ -6,11 +6,16 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 18:28:50 by snicolet          #+#    #+#             */
-/*   Updated: 2016/12/03 19:02:22 by snicolet         ###   ########.fr       */
+/*   Updated: 2016/12/03 19:37:27 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** return a new list duped from the first one, pointers are not copied, just
+** the address are copied, the user must send a valid compare function
+*/
 
 t_list	*ft_lstdupsort(t_list *lst, int (*cmp)(t_list *, t_list *))
 {

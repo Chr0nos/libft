25e0cb79a48f570b6eac127517f4a9afb9277f6b
Sébastien Nicolet <snicolet@student.42.fr>
2017-01-10 05:01:18 +0100

    norme

--- list/ft_lstissort_circular.c
+++ list/ft_lstissort_circular.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 20:15:26 by snicolet          #+#    #+#             */
-/*   Updated: 2017/01/10 04:58:42 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/01/10 05:00:59 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,8 @@ int		ft_lstissort_circular(t_list *lst, int (*cmp)(t_list *, t_list *))
 	lst = lst->next;
 	if (!lst)
 		return (1);
-	while ((lst->next) && ((cmp(lst, lst->next) == 0) || (cmp(lst->next, first) == 0)))
+	while ((lst->next) && ((cmp(lst, lst->next) == 0) ||
+			(cmp(lst->next, first) == 0)))
 		lst = lst->next;
 	if (lst->next)
 		return (0);

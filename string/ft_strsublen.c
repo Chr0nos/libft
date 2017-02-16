1ad827d13280652419924c687202dfa6b1eb9f94
Sébastien Nicolet <snicolet@student.42.fr>
2017-02-16 12:27:39 +0100

    fixed some serious shit

--- string/ft_strsublen.c
+++ string/ft_strsublen.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 21:33:55 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/12 20:56:21 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/02/16 12:27:24 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ size_t	ft_strsublen(const char *str, char c)
 	size_t	counter;
 
 	counter = 0;
+	if (!str)
+		return (0);
 	while ((*str) && (*str != c) && (str++))
 		counter++;
 	return (counter);

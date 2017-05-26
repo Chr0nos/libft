d7905c2d73240e76e1a6e3390dfdfef88aa49110
Chr0nos <snicolet@student.42.fr>
2017-05-26 15:17:03 +0000

    gestion de tailles de buffer plus petites avec ft_realloc

--- memory/ft_realloc.c
+++ memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/25 12:07:53 by snicolet          #+#    #+#             */
-/*   Updated: 2016/10/12 17:38:03 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/05/26 15:16:48 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,15 @@
 
 void	*ft_realloc(void *ptr, size_t size, size_t new_size)
 {
-	void	*dest;
+	void			*dest;
+	const size_t	copy_size = (size < new_size) ? size : new_size;
 
 	if (new_size < size)
 		return (0);
 	dest = malloc(new_size);
 	if (!dest)
 		return (0);
-	ft_memcpy(dest, ptr, size);
+	ft_memcpy(dest, ptr, copy_size);
 	free(ptr);
 	return (dest);
 }

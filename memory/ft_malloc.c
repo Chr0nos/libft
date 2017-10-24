7e4d9db0b6f0c70526909a1246ca0ea5dfe11dfd
Sebastien NICOLET <snicolet@student.42.fr>
2017-10-24 19:19:24 +0200

    push for debug

--- memory/ft_malloc.c
+++ memory/ft_malloc.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/23 15:55:06 by snicolet          #+#    #+#             */
-/*   Updated: 2017/10/24 18:11:09 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/10/24 18:59:06 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 
 /*
 ** configure a block of "size" and return the block address
+** returns the block itself
 */
 
 t_memblock		*ft_block_init(t_memblock *block, const size_t size)
@@ -28,7 +29,12 @@ t_memblock		*ft_block_init(t_memblock *block, const size_t size)
 	return (block);
 }
 
-void			ft_block_init_many(t_mempage *page, void *raw,
+/*
+** intitialize "count" blocks of "blocksize" contained into "page"
+** returns the next "free" block (for others calls to this)
+*/
+
+void			*ft_block_init_many(t_mempage *page, void *raw,
 	size_t const blocksize, size_t count)
 {
 	t_memblock		*block;
@@ -41,15 +47,18 @@ void			ft_block_init_many(t_mempage *page, void *raw,
 		raw = (void*)((size_t)raw + blocksize);
 		block++;
 	}
+	return (block);
 }
 
 /*
 ** memory management implementation:
+** t_mempage| t_memblock[]   |
 **    page  | blocks (meta)  |     raw memory pointed by blocks contents  |
 ** |--------|----------------|--------------------------------------------|
-** |   1    |       200      |                                            |
+** |   1    |       200      |        pointed by blocks->content          |
 ** |--------|----------------|--------------------------------------------|
-** |                                SIZE                                  |
+** |                                SIZE   (all the mapped memory size)   |
+** |        METADATA         |   (MEMTINY + MEMSMALL) or BIG  (rawsize)   |
 ** |--------|----------------|--------------------------------------------|
 */
 
@@ -62,21 +71,20 @@ t_mempage		*ft_page_create(t_mempage *parent)
 	t_mempage		*page;
 
 	rawsize = (MEMTINY * 100) + (MEMSMALL * 100);
-	// taille totale de la zone memoire a allouer
 	size = rawsize + (sizeof(t_memblock) * 200) + sizeof(t_mempage);
-	memory = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANON, -1, 0);
+	memory = mmap(NULL, size, PROT_WRITE | PROT_READ, MAP_PRIVATE | MAP_ANON,
+		-1, 0);
 	if (!memory)
 		return (NULL);
-	// la page est la premiere partie de la zone
 	page = (t_mempage*)(size_t)memory;
 	ft_bzero(page, sizeof(t_mempage));
 	page->blocks = (t_memblock*)((size_t)page + sizeof(t_mempage));
 	page->count = 200;
 	page->size = rawsize;
 	raw = (void*)((size_t)page->blocks + (sizeof(t_memblock) * page->count));
-	ft_block_init_many(page, raw, MEMSMALL, 100);
-	raw = (void*)((size_t)raw + (MEMSMALL * 100));
-	//ft_block_init_many(page, raw, MEMTINY, 100);
+	ft_printf("first raw: %p\n", raw);
+	raw = ft_block_init_many(page, raw, MEMSMALL, 100);
+	ft_block_init_many(page, raw, MEMTINY, 100);
 	page->prev = parent;
 	if (parent)
 		parent->next = page;
--- test_page.c
+++ test_page.c
@@ -2,18 +2,29 @@
 #include "ft_malloc.h"
 #include <sys/mman.h>
 
-int		main(void)
+static int		test_malloc_page(void)
 {
 	t_mempage	*page;
+	size_t		headsize;
 
+	headsize = sizeof(t_mempage) + (sizeof(t_memblock) * 200);
+	ft_printf("head size: %lu\n", headsize);
 	page = ft_page_create(NULL);
 	if (!page)
 	{
 		ft_putstr("epic failure\n");
 		return (1);
 	}
-	ft_printf("page size: %lu\nblocks: %lu\n", page->size, page->count);
+	ft_printf("page ptr %p\nsize: %lu\nblocks: %lu\n", page, page->size, page->count);
 	// purge de toute la zone "raw"
-	ft_bzero(page->blocks->content, page->size);
+	ft_printf("purging %p\n", page->blocks->content);
+	ft_bzero(page->blocks->content, page->size + 5744);
 	ft_page_delete(page);
+	return (0);
+}
+
+int		main(void)
+{
+	test_malloc_page();
+	return (0);
 }

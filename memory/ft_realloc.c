d7a0167bb976122caa1d5a3de0d5ed32439ce94b
Sébastien Nicolet <snicolet@student.42.fr>
2017-11-07 01:05:29 +0100

    debug

--- memory/ft_realloc.c
+++ memory/ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/08/25 12:07:53 by snicolet          #+#    #+#             */
-/*   Updated: 2017/11/07 00:58:02 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/11/07 01:05:14 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ static inline void	*ft_realloc_core(t_memblock *block, size_t size)
 	ptr = ft_malloc(size);
 	if (!ptr)
 	{
+		ft_putendl("failed to allocate memory");
 		ft_free(block->content);
 		return (NULL);
 	}
@@ -39,10 +40,14 @@ void				*ft_realloc(void *ptr, size_t size)
 	if (!size)
 	{
 		ft_free(ptr);
+		ft_putendl("no size");
 		return (NULL);
 	}
 	if (!ft_memfind(ptr, NULL, &block))
+	{
+		ft_putendl("failed to find item");
 		return (NULL);
+	}
 	if (block->size >= size)
 	{
 		block->used_size = size;

41c903e1ae4861b91e37539f051a1c8b0fd72321
Sébastien Nicolet <snicolet@student.42.fr>
2017-11-09 03:25:17 +0100

    netoyage de show_alloc_mem

--- memory/show_alloc_mem.c
+++ memory/show_alloc_mem.c
@@ -6,64 +6,42 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/06 19:05:49 by snicolet          #+#    #+#             */
-/*   Updated: 2017/11/07 19:17:36 by snicolet         ###   ########.fr       */
+/*   Updated: 2017/11/09 03:24:34 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static inline void	ft_malloc_showtype_title(char *title, const char *name,
-		const void *ptr)
+static const char	*gettitle(size_t const size)
 {
-	if (!*title)
-	{
-		ft_printf("%s%s\n", name, " : ", ptr);
-		*title = 1;
-	}
-
-}
-
-static size_t		ft_malloc_showtype(t_mempage *page, size_t const min,
-		size_t const max, const char * name)
-{
-	t_memblock	*block;
-	size_t		block_index;
-	size_t		total;
-	char		title;
-
-	total = 0;
-	title = 0;
-	block_index = 0;
-	while (block_index < page->count)
-	{
-		block = &page->blocks[block_index];
-		if ((block->used_size) &&
-				(page->blocksize > min) && (page->blocksize <= max))
-		{
-			ft_malloc_showtype_title(&title, name, block);
-			ft_printf("%p%s%p%s%lu%s",
-					block->content, " - ",
-					(size_t)block->content + page->blocksize,
-					" : ", block->used_size, " octects\n");
-			total += block->used_size;
-		}
-		block_index++;
-	}
-	return (total);
+	if (size == MEMTINY)
+		return ("TINY");
+	else if (size == MEMSMALL)
+		return ("SMALL");
+	return ("LARGE");
 }
 
 void		show_alloc_mem(void)
 {
-	size_t			total;
-	t_mempage		*page;
+	size_t		total;
+	size_t		p;
+	t_mempage	*page;
+	t_memblock	*block;
 
 	total = 0;
 	page = ft_page_store(NULL, READ);
 	while (page)
 	{
-		total += ft_malloc_showtype(page, 0, MEMTINY, "TINY") +
-			ft_malloc_showtype(page, MEMTINY, MEMSMALL, "SMALL") +
-			ft_malloc_showtype(page, MEMSMALL, (size_t)-1, "LARGE");
+		ft_printf("%s%s\n", gettitle(page->blocksize), " : ");
+		p = 0;
+		while (p < page->count)
+		{
+			block = &page->blocks[p++];
+			if (block->used_size > 0)
+				ft_printf("%p%s%p%s%lu%s", block->content, " - ",
+					(size_t)block->content + page->blocksize,
+					" : ", block->used_size, " octects\n");
+		}
 		page = page->next;
 	}
 	ft_printf("%s%lu%s", "Total : ", total, " octects\n");

5abd39200df21bf915b86208f1615c134a0c1555
Sébastien Nicolet <snicolet@student.42.fr>
2018-02-03 13:08:43 +0100

    gestoin du modifier skip pour la conversion %c du sscanf

--- scanf/conv/ft_scanf_set_char.c
+++ scanf/conv/ft_scanf_set_char.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/30 19:20:46 by snicolet          #+#    #+#             */
-/*   Updated: 2018/02/03 00:22:06 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/02/03 13:08:07 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,18 @@ int		ft_scanf_set_char(t_scanf *sf)
 	size_t			maxlen;
 	char			*str;
 
-	str = va_arg(*sf->ap, char*);
-	if (!str)
-	{
-		sf->flags |= FT_SF_QUIT | FT_SF_ERROR;
-		return (1);
-	}
 	maxlen = (sf->flags & FT_SF_FLAG_MAXLEN) ? (size_t)sf->maxlen : 1;
 	len = ft_strplen(sf->str, &sf->str[maxlen]);
-	ft_strncpy(str, sf->str, len);
+	if (!(sf->flags & FT_SF_FLAG_SKIP))
+	{
+		str = va_arg(*sf->ap, char*);
+		if (!str)
+		{
+			sf->flags |= FT_SF_QUIT | FT_SF_ERROR;
+			return (1);
+		}
+		ft_strncpy(str, sf->str, len);
+	}
 	sf->str += len;
 	return (1);
 }

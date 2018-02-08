f872477969e9021fa0e900f81eeb20c8eadb2b31
Sébastien Nicolet <snicolet@student.42.fr>
2018-02-08 02:01:41 +0100

    modification de ft_lstforeachi afin de qu elle puisse prendre un pointeur void* userdata et le passer

--- list/ft_lstforeachi.c
+++ list/ft_lstforeachi.c
@@ -3,24 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstforeachi.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: snicolet <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: snicolet <snicolet@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/15 19:12:26 by snicolet          #+#    #+#             */
-/*   Updated: 2015/12/15 19:24:37 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/02/08 02:00:52 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
-void	ft_lstforeachi(t_list *lst, void (*f)(size_t, size_t, void *))
+void	ft_lstforeachi(t_list *lst, void *userdata,
+	void (*f)(size_t, void *, size_t, void *))
 {
-	size_t	p;
+	size_t			p;
 
 	p = 0;
 	while (lst)
 	{
-		f(p++, lst->content_size, lst->content);
+		f(p++, userdata, lst->content_size, lst->content);
 		lst = lst->next;
 	}
 }

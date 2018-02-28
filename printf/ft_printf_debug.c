ad57759f88b4e30a5651d67da790503cc19d8102
Chr0nos <snicolet@student.42.fr>
2018-02-28 20:35:33 +0000

    affichage plus clair du debug

--- printf/ft_printf_debug.c
+++ printf/ft_printf_debug.c
@@ -6,7 +6,7 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/28 20:05:24 by snicolet          #+#    #+#             */
-/*   Updated: 2018/02/28 20:24:28 by snicolet         ###   ########.fr       */
+/*   Updated: 2018/02/28 20:28:35 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void		ft_printf_debug(const t_printf *pf)
 {
 	ft_printf("%s%s%p%s%b%s%d%s%d%s%d%s%d%s%lu%s%lu%s%lu%s",
 		"----- FT_PRINTF_DEBUG -----\n",
-		"va list ptr: ", pf->ap,
-		"\nflags: ", pf->flags,
-		"\nprecision: ", pf->precision,
-		"\nfd: ", pf->fd,
-		"\nmin_width: ", pf->min_width,
-		"\nraw len: ", pf->raw_len,
-		"\nsize: ", pf->size,
-		"\nspace left: ", pf->space_left,
+		"va list ptr:     ", pf->ap,
+		"\nflags:           ", pf->flags,
+		"\nprecision:       ", pf->precision,
+		"\nfd:              ", pf->fd,
+		"\nmin_width:       ", pf->min_width,
+		"\nraw len:         ", pf->raw_len,
+		"\nsize:            ", pf->size,
+		"\nspace left:      ", pf->space_left,
 		"\nbuffer max size: ", pf->buffer_maxsize,
 		"\n-------------------------\n");
 }

2100af015755ed0375831569382245d25c57c059
Chr0nos <snicolet@student.42.fr>
2019-05-10 12:57:40 +0200

    better codes are easiers ones, big simplification on ft_printf_stack, now the function does not spawn a sub structure for printf, it uses the orignal one, and just make a backup on the orignal va_list pointer

--- printf/ft_printf_stack.c
+++ printf/ft_printf_stack.c
@@ -6,64 +6,16 @@
 /*   By: snicolet <marvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/07 17:58:06 by snicolet          #+#    #+#             */
-/*   Updated: 2018/10/20 15:15:49 by snicolet         ###   ########.fr       */
+/*   Updated: 2019/05/10 12:56:26 by snicolet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "ft_printf.h"
-
 /*
-** setup a new children for pf, the children sepration's purpose is to prevent
-** any corruption inside the parent, the children's information are limited.
-** each children have it's own va_list.
-** only flags in FT_PF_ALLOW will be transmited
-*/
-
-static void					ft_printf_stack_init(const t_printf *pf,
-	t_printf *child, va_list *ap)
-{
-	ft_bzero(child, sizeof(*child));
-	child->ap = ap;
-	child->buffer = pf->buffer;
-	child->buff_asprintf = pf->buff_asprintf;
-	child->buff_start = &pf->buffer[pf->size];
-	child->flags = pf->flags & FT_PF_ALLOW;
-	child->space_left = pf->space_left;
-	child->buffer_maxsize = pf->buffer_maxsize;
-	child->fd = pf->fd;
-}
-
-/*
-** this function put some steps information to the parent t_printf, like
-** the new position in the buffer,
-** it is very important to call it at the end of a stacking of printf calls,
-** there is only one flag allowd to be push to the parent: FT_PF_ERROR,
-** if you need to transmit any data to the parent, use pf->raw_value, this one
-** will be peacefully given to parent.
-*/
-
-static void					ft_printf_stack_end(t_printf *pf,
-	const t_printf *child)
-{
-	pf->buff_asprintf = child->buff_asprintf;
-	pf->buff_start = child->buff_start;
-	pf->total_len += child->total_len;
-	pf->slen += child->size;
-	pf->space_left = child->space_left;
-	pf->size += child->size;
-	pf->raw_len += child->raw_len;
-	pf->flags |= child->flags & (FT_PF_QUIT | FT_PF_ERROR);
-	pf->raw_value = child->raw_value;
-}
-
-/*
-** this if probably the most complicated function of this printf implementation
 ** this function's purpose is to be called INSIDE a previous call of printf,
 ** like ft_printf, ft_snprintf etc, in a normal case this is called inside a
 ** callback function of printf (%k or %K)
-** this one make a new sub t_printf environement and re-run a ft_printf_engine
-** then the revelant informations are sent to the original structure.
 ** the big idea here is to prevent a maximum of "write" syscalls, and prevent
 ** too many stack usage (each ft_printf call causes a FT_PF_BSIZE buf in the
 ** stack, this is not acceptable for many calls inside a callback function)
@@ -74,12 +26,13 @@ int							ft_printf_stack(t_printf *pf,
 	const char *format, ...)
 {
 	va_list				ap;
-	t_printf			sub_pf;
+	va_list				*origin_ap;
 
+	origin_ap = pf->ap;
 	va_start(ap, format);
-	ft_printf_stack_init(pf, &sub_pf, &ap);
-	ft_printf_engine(format, &sub_pf);
+	pf->ap = &ap;
+	ft_printf_engine(format, pf);
 	va_end(ap);
-	ft_printf_stack_end(pf, &sub_pf);
-	return ((int)sub_pf.size);
+	pf->ap = origin_ap;
+	return ((int)pf->size);
 }
